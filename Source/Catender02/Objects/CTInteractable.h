13c87e0fafdc4fd1451fa4cf9d45c29fe558c9c2
Jan-Peter Wittig <134719731+CptTrips83@users.noreply.github.com>
2024-09-25 11:43:12 +0200

    Update CTInteractable.h
    Added Declaration for Overlapping-related Functions

--- Source/Catender02/Objects/CTInteractable.h
+++ Source/Catender02/Objects/CTInteractable.h
@@ -9,5 +9,28 @@ UCLASS()
 class CATENDER02_API ACTInteractable : public ACTSortable
 {
 	GENERATED_BODY()
-	
+
+	// TODO AddDynamic Binding to Capsule Component
+	UFUNCTION()
+	void OnBoxBeginOverlap
+	(
+		UPrimitiveComponent* OverlappedComp, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult
+	);
+
+	// TODO AddDynamic Binding to Capsule Component
+	UFUNCTION()
+	void OnBoxEndOverlap
+	(
+		UPrimitiveComponent* OverlappedComp, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult
+	);
 };

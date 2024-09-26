f8172475ae7d88161d7d5e633722c2ee8cb97bc2
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-09-26 17:43:33 +0200

    Add resource management features to CTWorldResourceComponent
    Introduced EResourceType enum and FResource, FResourceData structs. Implemented resource-related functions including GetResourceAmount, TryAddResourceAmount, and IsUnlockResource. Enhanced functionality with resource initialization and management logic in constructor and new methods.

--- Source/Catender02/Components/CTWorldResourceComponent.h
+++ Source/Catender02/Components/CTWorldResourceComponent.h
@@ -5,6 +5,37 @@
 #include "Components/ActorComponent.h"
 #include "CTWorldResourceComponent.generated.h"
 
+UENUM(BlueprintType)
+enum EResourceType
+{
+	Gold,
+	Meat,
+	Wood,
+	Stone,
+	Iron
+};
+
+USTRUCT(BlueprintType)
+struct FResource
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TEnumAsByte<EResourceType> ResourceType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 0, ClampMax = 20))
+	int Amount = 0;	
+};
+
+USTRUCT(BlueprintType)
+struct FResourceData
+{
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FResource Resource;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsUnlockResource = false;
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CATENDER02_API UCTWorldResourceComponent : public UActorComponent
@@ -17,8 +48,26 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	FResourceData* GetResourceData(const EResourceType ResourceType);
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int MaxResources = 30;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FResourceData> Resources;
+
+	UFUNCTION(BlueprintCallable)
+	int GetResourceAmount(const EResourceType ResourceType);
+	UFUNCTION(BlueprintCallable)
+	int TryAddResourceAmount(const EResourceType ResourceType, int Amount);
+	UFUNCTION(BlueprintCallable)
+	bool TrySubtractResourceAmount(const EResourceType ResourceType, int Amount);
+	UFUNCTION(BlueprintCallable)
+	bool IsUnlockResource(const EResourceType ResourceType);
+	UFUNCTION(BlueprintCallable)
+	int GetAmountAllResources();
+	UFUNCTION(BlueprintCallable)
+	int GetMaxResources();
 };
--- Source/Catender02/Components/CTWorldRessourceComponent.cpp
+++ Source/Catender02/Components/CTWorldRessourceComponent.cpp
@@ -4,6 +4,12 @@
 UCTWorldResourceComponent::UCTWorldResourceComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
+
+	Resources.Add(FResourceData(FResource(EResourceType::Gold, 0), false));
+	Resources.Add(FResourceData(FResource(EResourceType::Meat, 0), false));
+	Resources.Add(FResourceData(FResource(EResourceType::Wood, 0), true));
+	Resources.Add(FResourceData(FResource(EResourceType::Stone, 0), true));
+	Resources.Add(FResourceData(FResource(EResourceType::Iron, 0), true));
 }
 
 
@@ -12,9 +18,139 @@ void UCTWorldResourceComponent::BeginPlay()
 	Super::BeginPlay();
 }
 
+FResourceData* UCTWorldResourceComponent::GetResourceData(const EResourceType ResourceType)
+{
+	for (FResourceData& ResourceData : Resources)
+	{
+		if(ResourceData.Resource.ResourceType == ResourceType)
+		{
+			return &ResourceData;
+		}
+	}
+	return nullptr;
+}
+
 
 void UCTWorldResourceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+int UCTWorldResourceComponent::GetResourceAmount(const EResourceType ResourceType)
+{
+	const FResourceData* ResourceData = GetResourceData(ResourceType);
+
+	if (!ResourceData) return 0;	
+	
+	return ResourceData->Resource.Amount;
+}
+
+int UCTWorldResourceComponent::TryAddResourceAmount(const EResourceType ResourceType, const int Amount)
+{
+	// Erhalte die Daten der angeforderten Ressource
+	FResourceData* ResourceData = GetResourceData(ResourceType); 
+	if (!ResourceData)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Resource data not found."));
+		return 0;
+	}
+
+	// Überprüfe, ob die hinzuzufügende Menge negativ ist
+	if (Amount < 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Amount is negative."));
+		return 0;
+	}
+
+	int ReturningAmount = 0; // Variable für die Menge, die nach dem Hinzufügen verbleibt
+	int MaxResourceAmount = GetMaxResources(); // Maximal erlaubte Gesamtmenge an Ressourcen
+	int CurrentResourceAmount = GetAmountAllResources(); // Aktuelle Gesamtmenge aller Ressourcen
+
+	// Überprüfe die Konsistenz der Gesamtsummen
+	if (CurrentResourceAmount > MaxResourceAmount)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Current resource amount exceeds maximum allowed resources."));
+		return 0;
+	}
+
+	int RemainingResourceAmount = MaxResourceAmount - CurrentResourceAmount; // Der verfügbare Platz zur Speicherung von Ressourcen
+
+	// Überprüfe, ob die hinzuzufügende Menge den verbleibenden Platz übersteigt
+	if (Amount > RemainingResourceAmount)
+	{
+		ReturningAmount = Amount - RemainingResourceAmount; // Setze die verbleibende Menge, die nicht hinzugefügt werden konnte
+		ResourceData->Resource.Amount += RemainingResourceAmount; // Fülle den verfügbaren Platz vollständig aus
+	}
+	else
+	{
+		ResourceData->Resource.Amount += Amount; // Ansonsten füge die vollständige angeforderte Menge hinzu
+	}
+
+	// Überprüfen, dass die Ressourcensumme nicht negativ wird
+	if (ResourceData->Resource.Amount < 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Resulting resource amount is out of bounds."));
+		ResourceData->Resource.Amount = 0;
+		return 0;
+	}
+
+	return ReturningAmount; // Gebe die Menge zurück, die nicht hinzugefügt werden konnte
+}
+
+bool UCTWorldResourceComponent::TrySubtractResourceAmount(const EResourceType ResourceType, int Amount)
+{
+	bool Result = false;
+	FResourceData* ResourceData = GetResourceData(ResourceType);
+
+	if (!ResourceData)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Resource data not found."));
+		return false;
+	}
+
+	// Überprüfe, ob die hinzuzufügende Menge negativ ist
+	if (Amount < 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Amount is negative."));
+		return 0;
+	}
+
+	return Result;
+}
+
+bool UCTWorldResourceComponent::IsUnlockResource(const EResourceType ResourceType)
+{
+	const FResourceData* ResourceData = GetResourceData(ResourceType);	
+	
+	return !ResourceData ? false : ResourceData->IsUnlockResource;;
+}
+
+int UCTWorldResourceComponent::GetAmountAllResources()
+{
+	int Result = 0;
+
+	for (FResourceData ResourceData : Resources)
+	{
+		// Don't take Amount from UnlockResources
+		if (IsUnlockResource(ResourceData.Resource.ResourceType)) continue;
+
+		Result += ResourceData.Resource.Amount;
+	}
+
+	return Result;
+}
+
+int UCTWorldResourceComponent::GetMaxResources()
+{
+	int Result = 0;
+
+	for (FResourceData ResourceData : Resources)
+	{
+		if(IsUnlockResource(ResourceData.Resource.ResourceType)) continue;
+
+		Result += ResourceData.Resource.Amount;
+	}
+	
+	return Result;
 }
 

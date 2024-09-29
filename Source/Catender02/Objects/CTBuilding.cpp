ab2f88c4104475262bcbb5c5cec612bd66fb48fd
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-09-29 17:26:28 +0200

    Add CTBuilding and CTDestroyable classes
    Introduce two new actor classes, CTBuilding and CTDestroyable, with default properties and basic methods. These classes inherit from the base ACTBuildable class and include initialization, gameplay start, frame updates, and input binding functionalities.

---
+++ Source/Catender02/Objects/CTBuilding.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CTBuilding.h"
+
+
+// Sets default values
+ACTBuilding::ACTBuilding()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACTBuilding::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACTBuilding::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACTBuilding::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+

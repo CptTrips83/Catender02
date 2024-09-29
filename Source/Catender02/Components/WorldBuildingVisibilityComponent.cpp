85aa30d467919fc537553d4595bcebf4c7ecb985
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-09-29 17:47:48 +0200

    Add WorldBuildingVisibilityComponent for line-of-sight checks
    This component includes a function to determine if there's a clear line of sight between two points, accounting for blocking Actors. The functionality will help in building visibility checks for the game's world-building elements.

---
+++ Source/Catender02/Components/WorldBuildingVisibilityComponent.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "WorldBuildingVisibilityComponent.h"
+
+#include "Catender02/Objects/CTDestroyable.h"
+
+
+UWorldBuildingVisibilityComponent::UWorldBuildingVisibilityComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UWorldBuildingVisibilityComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void UWorldBuildingVisibilityComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                      FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+bool UWorldBuildingVisibilityComponent::HasLineOfSight(FVector Start, FVector End)
+{
+	TArray<FHitResult> Hit;
+	GetWorld()->LineTraceMultiByProfile(Hit, Start, End, "Buildable");
+
+	for (FHitResult HitResult : Hit)
+	{
+		if (!HitResult.GetActor()) continue;
+
+		if (Cast<ACTDestroyable>(HitResult.GetActor())) return false;
+	}
+
+	return true;
+}
---
+++ Source/Catender02/Components/WorldBuildingVisibilityComponent.h
@@ -0,0 +1,25 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WorldBuildingVisibilityComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CATENDER02_API UWorldBuildingVisibilityComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWorldBuildingVisibilityComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual bool HasLineOfSight(FVector Start, FVector End);
+};

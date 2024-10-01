af795e91f082d61e3ba1ba5b0a3e4ab392b397e1
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-01 21:31:49 +0200

    Add ACTPickable class for interactable objects
    Implemented a new ACTPickable class allowing objects to be picked up by the player character. Added functionalities for playing pickup/drop sounds and animations, disabling collisions, and applying resource effects.

---
+++ Source/Catender02/Objects/CTPickable.h
@@ -0,0 +1,56 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Catender02/CTSortable.h"
+#include "CTPickable.generated.h"
+
+
+
+UCLASS()
+class CATENDER02_API ACTPickable : public ACTSortable
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
+	TArray<FResource> Resources; 
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
+	USoundBase* PickupSound = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
+	USoundBase* DropSound = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
+	ECollisionChannel PickupChannel;
+	
+	void PlayPickupSound() const;
+	void PlayDropSound() const;
+	void PlayPickAnimation() const;
+	void DisableCollision() const;
+	void ApplyResources() const;
+
+	void DestroyPickup();
+	
+public:
+	ACTPickable();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	virtual void OnCapsuleBeginOverlap
+	(
+		UPrimitiveComponent* OverlappedComp, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult
+	);
+};

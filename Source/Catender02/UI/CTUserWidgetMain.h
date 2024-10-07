096d9e8a3563ae906bb9a5831a2c06fe4d956de4
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-07 17:21:57 +0200

    Add CTUserWidgetMain and CTWidgetComponent classes
    Introduced CTUserWidgetMain to handle owning interactables, and CTWidgetComponent to manage widget interactions. This setup establishes a communication link between the interactable owner and the main widget, enhancing modularity and code organization.

---
+++ Source/Catender02/UI/CTUserWidgetMain.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CTUserWidgetMain.generated.h"
+
+class ACTInteractable;
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class CATENDER02_API UCTUserWidgetMain : public UUserWidget
+{
+	GENERATED_BODY()
+
+	ACTInteractable* OwningInteractable;
+
+public:
+	UFUNCTION(BlueprintPure)
+	ACTInteractable* GetOwningInteractable() const;
+	
+	void SetOwningInteractable(ACTInteractable* Interactable);
+};
---
+++ Source/Catender02/UI/CTWidgetComponent.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CTWidgetComponent.h"
+
+#include "CTUserWidgetMain.h"
+#include "Catender02/Objects/CTInteractable.h"
+
+
+// Sets default values for this component's properties
+UCTWidgetComponent::UCTWidgetComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UCTWidgetComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+    UCTUserWidgetMain* MainWidget = Cast<UCTUserWidgetMain>(GetWidget());
+	if (!MainWidget) return;
+	ACTInteractable* Interactable = Cast<ACTInteractable>(GetOwner());
+	if (!Interactable) return;	
+	MainWidget->SetOwningInteractable(Interactable);
+}
+
+
+// Called every frame
+void UCTWidgetComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+

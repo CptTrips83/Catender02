22ec8eec4358538cbe04b82abf6836f5047d26d6
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-15 21:38:00 +0200

    Create CTBuildingWorkComponent
    Add a new UCTBuildingWorkComponent class with methods to manage friendly NPC characters working in a building. This includes tracking, adding, and removing NPCs, and monitoring building state changes. Additionally, it integrates with the building component and counts available workplaces.

---
+++ Source/Catender02/Components/CTBuildingWorkComponent.cpp
@@ -0,0 +1,84 @@
+﻿
+
+#include "CTBuildingWorkComponent.h"
+
+#include "Catender02/Objects/CTBuildable.h"
+
+
+UCTBuildingWorkComponent::UCTBuildingWorkComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UCTBuildingWorkComponent::AddFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character)
+{
+	if (!Character)
+	{
+		return;
+	}
+
+	WorkingFriendlyNPCCharacters.Add(Character);
+}
+
+void UCTBuildingWorkComponent::RemoveFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character)
+{
+	if (!Character)
+	{
+		return;
+	}
+
+	WorkingFriendlyNPCCharacters.Remove(Character);
+}
+
+bool UCTBuildingWorkComponent::HasFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character)
+{
+	if (!Character)
+	{
+		return false;
+	}
+
+	return WorkingFriendlyNPCCharacters.Contains(Character);
+}
+
+int UCTBuildingWorkComponent::CountFriendlyNPCCharacters()
+{
+	return WorkingFriendlyNPCCharacters.Num();
+}
+
+bool UCTBuildingWorkComponent::HasOpenWorkPlace()
+{
+	return WorkingFriendlyNPCCharacters.Num() < AmountWorkPlaces;
+}
+
+
+void UCTBuildingWorkComponent::GetBuildingComponentFromOwner()
+{
+	if (ACTBuildable* Buildable = Cast<ACTBuildable>(GetOwner()))
+	{
+		BuildingComponent = Buildable->GetBuildingComponent();
+		BuildingComponent->OnBuildingStateChanged.AddDynamic(
+			this,
+			&UCTBuildingWorkComponent::BuildingStateChanged
+		);
+	}	
+}
+
+void UCTBuildingWorkComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetBuildingComponentFromOwner();
+}
+
+void UCTBuildingWorkComponent::BuildingStateChanged(ACTBuildable* Buildable, EBuildingState OldState,
+	EBuildingState NewState)
+{
+	// Clear NPC Array
+}
+
+void UCTBuildingWorkComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ Source/Catender02/Components/CTBuildingWorkComponent.h
@@ -0,0 +1,43 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CTBuildingComponent.h"
+#include "Catender02/Character/CTFriendlyNPCCharacter.h"
+#include "Components/ActorComponent.h"
+#include "CTBuildingWorkComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CATENDER02_API UCTBuildingWorkComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	TArray<ACTFriendlyNPCCharacter*> WorkingFriendlyNPCCharacters;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	int AmountWorkPlaces = 1;
+
+	UCTBuildingComponent* BuildingComponent = nullptr;
+	
+public:
+	UCTBuildingWorkComponent();
+
+	virtual void AddFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
+	virtual void RemoveFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
+
+	virtual bool HasFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
+	virtual int CountFriendlyNPCCharacters();
+	virtual bool HasOpenWorkPlace();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void BuildingStateChanged(ACTBuildable* Buildable, EBuildingState OldState, EBuildingState NewState);
+	
+	void GetBuildingComponentFromOwner();
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};

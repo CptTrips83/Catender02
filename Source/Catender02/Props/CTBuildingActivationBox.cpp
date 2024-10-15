fa1daefafe096f433efe2ad337bc12faa9eeb7b7
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-15 17:22:45 +0200

    Add CTBuildingActivationBox for building visibility control
    Implemented the CTBuildingActivationBox class to manage building activation and visibility. It includes methods for handling overlap interactions and dynamically adjusting the visibility box size based on overlapping destroyables. The Box component's position and extent are updated in real-time during gameplay.

---
+++ Source/Catender02/Props/CTBuildingActivationBox.cpp
@@ -0,0 +1,108 @@
+﻿
+
+#include "CTBuildingActivationBox.h"
+
+#include "Catender02/Objects/CTBuilding.h"
+
+
+void ACTBuildingActivationBox::GrowBox()
+{
+	FVector NewBoxExtent = FVector(
+		GetBoxBuildingVisibilityComponent()->GetUnscaledBoxExtent().X + CurrentBuildingVisibilityGrowSpeed,
+		GetBoxBuildingVisibilityComponent()->GetUnscaledBoxExtent().Y,
+		GetBoxBuildingVisibilityComponent()->GetUnscaledBoxExtent().Z
+	);
+	
+	GetBoxBuildingVisibilityComponent()->SetBoxExtent(NewBoxExtent, true);
+
+	float CurrentGrowExtentX = GetBoxBuildingVisibilityComponent()->GetUnscaledBoxExtent().X - InitialXBoxExtent;
+
+	CurrentGrowExtentX *= GrowDirection == Right ? 1 : -1;
+	
+	FVector NewRelativeLocation = FVector(
+		InitialRelativeLocation.X + (CurrentGrowExtentX),
+		InitialRelativeLocation.Y,
+		InitialRelativeLocation.Z
+	);
+
+	GetBoxBuildingVisibilityComponent()->SetRelativeLocation(NewRelativeLocation);
+}
+
+ACTBuildingActivationBox::ACTBuildingActivationBox()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxBuildingVisibilityComponent = CreateDefaultSubobject<UBoxComponent>("Box Building Visibility");
+	BoxBuildingVisibilityComponent->SetupAttachment(GetRootComponent());
+}
+
+UBoxComponent* ACTBuildingActivationBox::GetBoxBuildingVisibilityComponent()
+{
+	return BoxBuildingVisibilityComponent;
+}
+
+void ACTBuildingActivationBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetBoxBuildingVisibilityComponent()->OnComponentBeginOverlap.AddDynamic(
+		this,
+		&ACTBuildingActivationBox::OnBoxBeginOverlapInteraction
+	);
+
+	GetBoxBuildingVisibilityComponent()->OnComponentEndOverlap.AddDynamic(
+		this,
+		&ACTBuildingActivationBox::OnBoxEndOverlapInteraction
+	);
+
+	OnOverlappingDestroyablesChanged.AddDynamic(this, &ACTBuildingActivationBox::OverlappingDestroyablesChanged);
+
+	InitialRelativeLocation = GetBoxBuildingVisibilityComponent()->GetRelativeLocation();
+	InitialXBoxExtent = GetBoxBuildingVisibilityComponent()->GetScaledBoxExtent().X;
+
+	OverlappingDestroyablesChanged();
+}
+
+void ACTBuildingActivationBox::OnBoxBeginOverlapInteraction(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor) return;	
+	if (ACTBuilding* Building = Cast<ACTBuilding>(OtherActor))
+	{
+		Building->SetActive(true);
+	}
+	if (ACTDestroyable* Destroyable = Cast<ACTDestroyable>(OtherActor))
+	{
+		OverlappingDestroyables.Add(Destroyable);
+		OnOverlappingDestroyablesChanged.Broadcast();
+	}
+}
+
+void ACTBuildingActivationBox::OnBoxEndOverlapInteraction(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (ACTDestroyable* Destroyable = Cast<ACTDestroyable>(OtherActor))
+	{
+		OverlappingDestroyables.Remove(Destroyable);
+		OnOverlappingDestroyablesChanged.Broadcast();
+	}
+}
+
+void ACTBuildingActivationBox::OverlappingDestroyablesChanged()
+{
+	if (OverlappingDestroyables.Num() > 0)
+	{
+		CurrentBuildingVisibilityGrowSpeed = 0;
+	}
+	else
+	{
+		CurrentBuildingVisibilityGrowSpeed = MaxBuildingVisibilityGrowSpeed;
+	}
+}
+
+void ACTBuildingActivationBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	GrowBox();
+}
+

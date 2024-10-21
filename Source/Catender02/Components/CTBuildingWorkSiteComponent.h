22e59a653af8dc068c3530c6b3aeae5043998445
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-21 20:39:16 +0200

    Add detailed documentation for functions and properties
    Extended class documentation to include descriptions for properties and methods. This aids in clarity for developers, ensuring better maintainability and comprehensibility of the component's functionalities.

--- Source/Catender02/Components/CTBuildingWorkSiteComponent.h
+++ Source/Catender02/Components/CTBuildingWorkSiteComponent.h
@@ -8,16 +8,57 @@
 #include "CTBuildingWorkSiteComponent.generated.h"
 
 
+/**
+ * A component that manages the work site for a building, including the assignment and tracking
+ * of friendly NPC characters working on the site.
+ *
+ * This class is responsible for maintaining the state of the building work site, managing work places,
+ * and controlling the friendly NPC characters assigned to work on the building. It interacts with other
+ * building components to update and respond to changes in the building's state.
+ */
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class CATENDER02_API UCTBuildingWorkSiteComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	/**
+	 * A list of friendly NPC characters currently working on the building work site.
+	 *
+	 * This array holds pointers to instances of ACTFriendlyNPCCharacter that are
+	 * designated as working on the building work site. It is used to manage and
+	 * track the friendly NPC characters involved with the work site.
+	 *
+	 * This property is visible anywhere and can be read in Blueprint scripts but is
+	 * privately accessible within its own class. This restriction is enforced through
+	 * the use of the "AllowPrivateAccess=true" metadata specifier.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TArray<ACTFriendlyNPCCharacter*> WorkingFriendlyNPCCharacters;
 
+	/**
+	 * The number of total available work places on the building work site.
+	 *
+	 * This integer value represents the maximum number of work places available for NPC characters on the building work site.
+	 * It is utilized to determine if additional NPC characters can be assigned to work on the work site.
+	 *
+	 * This property can be edited only in the defaults and read in Blueprint scripts but is privately accessible within its own class.
+	 * This access restriction is controlled through the "AllowPrivateAccess=true" metadata specifier.
+	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	int AmountWorkPlaces = 1;
 
+	/**
+	 * A map associating building states with NPC character classes designated as workers.
+	 *
+	 * This map holds keys of type TEnumAsByte<EBuildingState>, representing different states of a building,
+	 * and values of type TSubclassOf<ACTFriendlyNPCCharacter>, representing classes of friendly NPC characters
+	 * that can work in those states. It is used to manage and map the types of NPC workers to the specific
+	 * states of the building work site.
+	 *
+	 * This property is editable only in the defaults and can be read in Blueprint scripts, but is privately
+	 * accessible within its own class. This access restriction is managed through the "AllowPrivateAccess=true"
+	 * metadata specifier.
+	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	TMap<TEnumAsByte<EBuildingState>, TSubclassOf<ACTFriendlyNPCCharacter>> BuildingStatesWorker;
 	
@@ -25,15 +66,60 @@ class CATENDER02_API UCTBuildingWorkSiteComponent : public UActorComponent
 	ACTBuildable* OwningBuildable = nullptr;
 	
 public:
-	UCTBuildingWorkSiteComponent();	
+	UCTBuildingWorkSiteComponent();
 
+	/**
+	 * Adds a friendly NPC character to the working friendly NPC characters list.
+	 *
+	 * Appends the given friendly NPC character pointer to the list of
+	 * working friendly NPC characters if it is not null.
+	 *
+	 * @param Character A pointer to the friendly NPC character to be added.
+	 */
+	UFUNCTION(BlueprintCallable)
 	virtual void AddFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
+	/**
+	 * Removes a friendly NPC character from the working friendly NPC characters list.
+	 *
+	 * Removes the given friendly NPC character pointer from the list of
+	 * working friendly NPC characters if it is not null.
+	 *
+	 * @param Character A pointer to the friendly NPC character to be removed.
+	 */
+	UFUNCTION(BlueprintCallable)
 	virtual void RemoveFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
 
+	/**
+	 * Determines whether a given friendly NPC character is part of the working friendly NPC characters list.
+	 *
+	 * Checks the provided friendly NPC character pointer against a list of
+	 * working friendly NPC characters to see if it is included.
+	 *
+	 * @param Character A pointer to the friendly NPC character to check.
+	 * @return true if the friendly NPC character is part of the working friendly NPC characters list, false otherwise.
+	 */
+	UFUNCTION(BlueprintPure)
 	virtual bool HasFriendlyNPCCharacter(ACTFriendlyNPCCharacter* Character);
+	/**
+	 * Counts the number of friendly NPC characters currently working.
+	 *
+	 * Retrieves the number of friendly NPC characters that are currently part
+	 * of the working friendly NPC characters list.
+	 *
+	 * @return The number of friendly NPC characters currently working.
+	 */
+	UFUNCTION(BlueprintPure)
 	virtual int CountFriendlyNPCCharacters();
+	/**
+	 * Checks if there is an open work place available in the building work site.
+	 *
+	 * Evaluates the number of currently working NPC characters against the total number
+	 * of available work places to determine if there is an open position.
+	 *
+	 * @return true if there is at least one open work place, false otherwise.
+	 */
+	UFUNCTION(BlueprintPure)
 	virtual bool HasOpenWorkPlace();
-	void AddBuildingStateChangedListener();
 
 protected:
 	virtual void BeginPlay() override;
@@ -43,6 +129,7 @@ protected:
 
 	void GetBuildingComponentFromOwner();
 	void GetOwningBuildableFromOwner();
+	void AddBuildingStateChangedListener();
 
 	/**
 	 * Determines if a given NPC character is associated with an active work site.

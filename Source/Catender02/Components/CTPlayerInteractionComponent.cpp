42ff806377a33297859936bce01e23df01a74332
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-22 20:21:07 +0200

    Add detailed comments to interaction component methods
    Enhanced code documentation by adding detailed comments for all main functions in CTPlayerInteractionComponent. These comments describe the purpose, behavior, and parameters of each function, improving code clarity and maintainability.

--- Source/Catender02/Components/CTPlayerInteractionComponent.cpp
+++ Source/Catender02/Components/CTPlayerInteractionComponent.cpp
@@ -3,16 +3,6 @@
 #include "Catender02/Objects/CTInteractable.h"
 #include "Kismet/KismetSystemLibrary.h"
 
-/**
- * Sorts the interactable objects in the `OverlappingInteractables` array
- * based on their translucency sort priority, in descending order.
- *
- * This function uses a lambda comparator to compare the `TranslucencySortPriority`
- * property of the sprites associated with the `ACTInteractable` objects.
- *
- * It ensures that interactables with higher translucency sort priority
- * appear before those with lower priority.
- */
 void UCTPlayerInteractionComponent::SortInteractablesBySortingLayer()
 {
 	OverlappingInteractables.Sort([](const ACTInteractable& A, const ACTInteractable& B) {
@@ -20,13 +10,6 @@ void UCTPlayerInteractionComponent::SortInteractablesBySortingLayer()
 	});
 }
 
-/**
- * Refreshes the highlighting state of interactable objects in the `OverlappingInteractables` array.
- *
- * This function iterates through all interactable objects that are currently overlapping
- * and disables their highlight. It then highlights the interactable object that is at
- * the front of the array.
- */
 void UCTPlayerInteractionComponent::RefreshInteractables()
 {
 	for(ACTInteractable* Interactable : OverlappingInteractables)
@@ -53,14 +36,6 @@ UCTPlayerInteractionComponent::UCTPlayerInteractionComponent()
 	OnRemovedInteractable.AddDynamic(this, &UCTPlayerInteractionComponent::RemovedInteractable);
 }
 
-/**
- * Adds an interactable object to the `OverlappingInteractables` array,
- * ensuring it is added uniquely. The array is then sorted by the
- * translucency sort priority of the interactables in descending order.
- * Finally, the `OnAddedInteractable` event is broadcast with the added interactable.
- *
- * @param Interactable The interactable object to be added to the array.
- */
 void UCTPlayerInteractionComponent::AddToOverlappingInteractables(ACTInteractable* Interactable)
 {	
 	this->OverlappingInteractables.AddUnique(Interactable);
@@ -70,14 +45,6 @@ void UCTPlayerInteractionComponent::AddToOverlappingInteractables(ACTInteractabl
 	UKismetSystemLibrary::PrintString(this, "Added Interactable" + Interactable->GetName());
 }
 
-/**
- * Removes the specified interactable from the `OverlappingInteractables` array.
- *
- * This function also sorts the remaining interactables by their translucency sort priority
- * and broadcasts an event indicating that an interactable has been removed.
- *
- * @param Interactable The interactable object to remove from the overlapping interactables list.
- */
 void UCTPlayerInteractionComponent::RemoveFromOverlappingInteractables(ACTInteractable* Interactable)
 {
 	this->OverlappingInteractables.Remove(Interactable);
@@ -87,11 +54,6 @@ void UCTPlayerInteractionComponent::RemoveFromOverlappingInteractables(ACTIntera
 	UKismetSystemLibrary::PrintString(this, "Removed Interactable" + Interactable->GetName());
 }
 
-/**
- * Retrieves the interactable object that is at the front of the `OverlappingInteractables` array.
- *
- * @return A pointer to the front `ACTInteractable` object if the array is not empty; otherwise, returns nullptr.
- */
 ACTInteractable* UCTPlayerInteractionComponent::GetFrontInteractable()
 {	
 	return this->OverlappingInteractables.Num() > 0 ? this->OverlappingInteractables[0] : nullptr;	
--- Source/Catender02/Components/CTPlayerInteractionComponent.h
+++ Source/Catender02/Components/CTPlayerInteractionComponent.h
@@ -24,27 +24,68 @@ class CATENDER02_API UCTPlayerInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-	TArray<ACTInteractable*> OverlappingInteractables;	
+	TArray<ACTInteractable*> OverlappingInteractables;
 
+	/**
+	 * Sorts the list of overlapping interactables by their translucency sort priority, in descending order.
+	 * Only interactables that can be interacted with are considered in the sorting process.
+	 */
 	void SortInteractablesBySortingLayer();
 
+	/**
+	 * Refreshes the list of interactables by resetting their highlight state and interaction widget visibility.
+	 * For the interactable at the front, it will highlight and enable its interaction widget if it can be interacted with.
+	 */
 	void RefreshInteractables();
 	
 public:	
 	UCTPlayerInteractionComponent();
 
+	/**
+	 * Adds an interactable item to the list of currently overlapping interactables.
+	 * This method also sorts the list of interactables by their translucency sort priority
+	 * and triggers the OnAddedInteractable event.
+	 *
+	 * @param Interactable The interactable item to be added to the overlapping list.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void AddToOverlappingInteractables(ACTInteractable* Interactable);
+	/**
+	 * Removes an interactable item from the list of currently overlapping interactables.
+	 * This method also sorts the list of remaining interactables by their translucency sort priority
+	 * and triggers the OnRemovedInteractable event.
+	 *
+	 * @param Interactable The interactable item to be removed from the overlapping list.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void RemoveFromOverlappingInteractables(ACTInteractable* Interactable);
+	/**
+	 * Gets the interactable object that is currently at the front of the interaction list.
+	 * The front interactable is the first item in the list of overlapping interactables.
+	 *
+	 * @return The front interactable if available, otherwise returns nullptr.
+	 */
 	UFUNCTION(BlueprintCallable)
 	ACTInteractable* GetFrontInteractable();	
 	
 protected:
 	virtual void BeginPlay() override;
 
+	/**
+	 * Handles the logic when a new interactable is added to the interaction component.
+	 * This method refreshes the list of interactables to ensure it is updated.
+	 *
+	 * @param Interactable The interactable item that is added to the component.
+	 */
 	UFUNCTION()
 	void AddedInteractable(ACTInteractable* Interactable);
+	/**
+	 * Handles the logic when an interactable is removed from the interaction component.
+	 * This method disables the highlight and visibility of the interaction widget for the removed interactable
+	 * and refreshes the list of interactables to ensure it is updated.
+	 *
+	 * @param Interactable The interactable item that is removed from the component.
+	 */
 	UFUNCTION()
 	void RemovedInteractable(ACTInteractable* Interactable);
 

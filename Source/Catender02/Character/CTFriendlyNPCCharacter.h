8d7cbfb54fce3423593bf53032f2bd1cd1d92d82
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-24 21:26:48 +0200

    Enhance documentation and add new NPC methods
    Added detailed doc comments for better understanding of NPC interactions with building work sites. Introduced new methods to assign, withdraw, and find the nearest work site for NPC characters.

--- Source/Catender02/Character/CTFriendlyNPCCharacter.h
+++ Source/Catender02/Character/CTFriendlyNPCCharacter.h
@@ -7,11 +7,18 @@
 
 class ACTBuilding;
 
+/**
+ * Represents a friendly non-playable character (NPC) within the game world that can interact with building work sites.
+ */
 UCLASS()
 class CATENDER02_API ACTFriendlyNPCCharacter : public ACTCharacter
 {
 	GENERATED_BODY()
 
+	/**
+	 * Pointer to a buildable object representing the current building work site assigned to the friendly NPC character.
+	 */
+	UPROPERTY()
 	ACTBuildable* AssignedBuildable;
 	
 public:
@@ -25,11 +32,26 @@ public:
 
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
+	/**
+	 * Assigns the friendly NPC character to a specified building work site.
+	 *
+	 * @param Buildable A pointer to the buildable object representing the work site to assign the NPC to.
+	 */
 	UFUNCTION(BlueprintCallable)
 	virtual void AssignToBuildingWorkSite(ACTBuildable* Building);
+	/**
+	 * Withdraws the friendly NPC character from a specified building work site.
+	 *
+	 * @param Buildable A pointer to the buildable object representing the work site to withdraw the NPC from.
+	 */
 	UFUNCTION(BlueprintCallable)
 	virtual void WithdrawFromBuildingWorkSite(ACTBuildable* Building);
 
+	/**
+	 * Retrieves the nearest working site for the friendly NPC character.
+	 *
+	 * @return A pointer to the nearest ACTBuildable object representing the work site, or nullptr if no suitable site is found.
+	 */
 	UFUNCTION(BlueprintPure)
 	virtual ACTBuildable* GetNearestWorkingSite();
 };

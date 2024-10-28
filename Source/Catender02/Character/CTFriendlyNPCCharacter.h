5eab7dc87cd8f770c6bd79a38618a321d8f57f2f
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-10-28 21:39:02 +0100

    Add worksite capsule and home building to NPC character
    Introduced a capsule component for work site detection and added a soft pointer for home building references. Implemented support for Overlap events and functions for managing NPC states such as waiting and working.

--- Source/Catender02/Character/CTFriendlyNPCCharacter.h
+++ Source/Catender02/Character/CTFriendlyNPCCharacter.h
@@ -43,6 +43,12 @@ class CATENDER02_API ACTFriendlyNPCCharacter : public ACTCharacter
 	UPROPERTY()
 	bool IsWorking = false;
 
+	/**
+	 * A soft pointer to an ACTBuilding object representing the home building associated with the friendly NPC character.
+	 *
+	 * This variable allows the assignment and retrieval of a home building in a blueprint-accessible manner.
+	 * It also provides the flexibility of referencing an asset without loading it immediately.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
 	TSoftObjectPtr<ACTBuilding> HomeBuilding;
 
@@ -51,22 +57,60 @@ class CATENDER02_API ACTFriendlyNPCCharacter : public ACTCharacter
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
 	FTimerHandle WaitingTimerHandle;
+
+	/**
+	 * Capsule component representing the work site area associated with the friendly NPC character.
+	 * Used to detect overlaps and interactions specific to work sites.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess=true))
+	UCapsuleComponent* WorkSiteCapsule;
+
+	/**
+	 * Resets the collision settings for the work site capsule associated with the friendly NPC character.
+	 * The capsule component's collision is first disabled and then set to query-only mode, ensuring that
+	 * it can detect overlaps without affecting physics interactions.
+	 */
+	void ResetWorkSiteCapsule();
 	
 public:
 	ACTFriendlyNPCCharacter();
 
 protected:
 	virtual void BeginPlay() override;
-	
+
+	/**
+	 * Initiates the working process for the friendly NPC character.
+	 * This method is intended to be implemented in Blueprint and can define custom behavior when the NPC starts working.
+	 */
 	UFUNCTION(BlueprintImplementableEvent)
 	void StartWorking();
 
+	/**
+	 * Processes ongoing work for the friendly NPC character.
+	 * This method can be overridden in Blueprint to define custom behavior during the NPC's working period.
+	 */
 	UFUNCTION(BlueprintNativeEvent)
 	void ProcessWorking();
-	
+
+	/**
+	 * Stops the working process for the friendly NPC character.
+	 * This method is intended to be implemented in Blueprint and can define custom behavior when the NPC stops working.
+	 */
 	UFUNCTION(BlueprintImplementableEvent)	
 	void StopWorking();
 
+	/**
+	 * Handles the begin overlap event for the work site detection capsule component.
+	 * This method is triggered when another actor begins overlapping with the work site capsule component,
+	 * primarily to check if the overlapping actor is a buildable work site and to start the working process accordingly.
+	 *
+	 * @param OverlappedComp The work site capsule component that triggered the overlap event.
+	 * @param OtherActor The actor that is overlapping with the work site capsule component.
+	 * @param OtherComp The specific component on the other actor that is overlapping with the work site capsule.
+	 * @param OtherBodyIndex The index of the overlapping body.
+	 * @param bFromSweep Whether the overlap event occurred from a sweeping movement.
+	 * @param SweepResult The hit result data if the overlap event was caused by a sweep.
+	 */
 	UFUNCTION()
 	virtual void OnWorkSiteBoxBeginOverlap
 	(
@@ -78,6 +122,15 @@ protected:
 		const FHitResult& SweepResult
 	);
 
+	/**
+	 * Handles the end overlap event for the work site detection capsule component.
+	 * This method is triggered when another actor stops overlapping with the work site capsule component.
+	 *
+	 * @param OverlappedComp The work site capsule component that triggered the overlap event.
+	 * @param OtherActor The actor that was overlapping with the work site capsule component.
+	 * @param OtherComp The specific component on the other actor that was overlapping with the work site capsule.
+	 * @param OtherBodyIndex The index of the overlapping body.
+	 */
 	UFUNCTION()
 	virtual void OnWorkSiteBoxEndOverlap
 	(
@@ -92,6 +145,10 @@ public:
 
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
+	/**
+	 * Retrieves the currently assigned buildable object that the friendly NPC is associated with.
+	 * @return The buildable object assigned to the NPC.
+	 */
 	UFUNCTION(BlueprintPure)
 	virtual ACTBuildable* GetAssignedBuildable();
 	
@@ -118,33 +175,78 @@ public:
 	UFUNCTION(BlueprintPure)
 	virtual ACTBuildable* GetNearestWorkingSite();
 
+	/**
+	 * Sets the waiting state of the friendly non-playable character (NPC).
+	 *
+	 * @param NewValue The new waiting state to be set.
+	 */
 	UFUNCTION(BlueprintCallable)
 	virtual void SetIsWaiting(bool NewValue);
 
+	/**
+	 * Checks if the friendly NPC character is currently waiting.
+	 *
+	 * @return True if the NPC character is waiting, otherwise false.
+	 */
 	UFUNCTION(BlueprintPure)
 	virtual bool GetIsWaiting();
 
+	/**
+	 * Sets the working state of the friendly NPC character.
+	 *
+	 * @param NewValue A boolean indicating whether the NPC is currently working.
+	 */
 	UFUNCTION(BlueprintCallable)
 	virtual void SetIsWorking(bool NewValue);
 
+	/**
+	 * Checks if the NPC character is currently working at a building work site.
+	 *
+	 * @return true if the NPC is working, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure)
 	virtual bool GetIsWorking();
 
+	/**
+	 * Sets the home building for the friendly NPC character.
+	 *
+	 * @param Building A pointer to the building that will be set as the NPC character's home.
+	 */
 	UFUNCTION(BlueprintCallable)
 	virtual void SetHomeBuilding(ACTBuilding* Building);
 
+	/**
+	 * Retrieves the home building associated with this friendly NPC character.
+	 *
+	 * @return The home building as a soft object pointer.
+	 */
 	UFUNCTION(BlueprintPure)
 	virtual TSoftObjectPtr<ACTBuilding> GetHomeBuilding();
 
+	/**
+	 * Retrieves the capsule component representing the work site associated with this friendly NPC character.
+	 *
+	 * @return A pointer to the capsule component that defines the work site.
+	 */
 	UFUNCTION(BlueprintPure)
 	UCapsuleComponent* GetWorkSiteCapsule();
 	
 	//UFUNCTION(BlueprintPure)
 	//virtual float GetRandomPositionInBox(USceneComponent* Box);
-	
+
+	/**
+	 * Event delegate that is called when the waiting state changes.
+	 *
+	 * This delegate is marked as BlueprintAssignable, meaning it can be assigned
+	 * to events or functions in Blueprint to handle changes in the waiting state
+	 * of an object or character.
+	 */
 	UPROPERTY(BlueprintAssignable)
 	FWaitingIsChanged OnWaitingIsChanged;
 
+	/**
+	 * Event delegate triggered when the working status changes.
+	 */
 	UPROPERTY(BlueprintAssignable)
 	FWorkingIsChanged OnWorkingIsChanged;
 };

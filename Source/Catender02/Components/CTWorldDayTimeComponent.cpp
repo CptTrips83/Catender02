62cf68ac59c3e5af55b33f559bf6e344a8c22c95
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-11-11 23:01:31 +0100

    Add UCTWorldDayTimeComponent to manage game time progression
    Introduced UCTWorldDayTimeComponent.cpp and .h to facilitate real-time day and night cycle management in the game. Component tracks current minute, hour, and day, broadcasting events when day time and day change. Includes helper functions to determine if it's daytime and calculate total day length.

---
+++ Source/Catender02/Components/CTWorldDayTimeComponent.cpp
@@ -0,0 +1,49 @@
+﻿
+#include "CTWorldDayTimeComponent.h"
+
+
+UCTWorldDayTimeComponent::UCTWorldDayTimeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UCTWorldDayTimeComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+void UCTWorldDayTimeComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	CurrentMinute += DeltaTime;
+
+	if (CurrentMinute < HourLength) return;
+
+	const int OldHour = CurrentMinute;
+	const int OldDay = CurrentDay;
+	const int OldIsDay = IsDay();
+	
+	if (CurrentMinute >= HourLength)
+	{		
+		CurrentMinute = 0;
+		CurrentHour++;
+
+		OnDayTimeChanged.Broadcast(OldHour, CurrentHour, OldIsDay, IsDay());
+	}
+
+	if (CurrentMinute > GetDayLength())
+	{
+		CurrentHour = 0;
+		CurrentDay++;
+
+		OnDayChanged.Broadcast(OldDay, CurrentDay, OldIsDay, IsDay());
+	}
+}
+
+bool UCTWorldDayTimeComponent::IsDay()
+{
+	return true;
+}
+

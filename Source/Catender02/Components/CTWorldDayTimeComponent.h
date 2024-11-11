cdbcc5c17b4d5bfb411f284179712c0b519dbfe8
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-11-11 23:01:31 +0100

    Add UCTWorldDayTimeComponent to manage game time progression
    Introduced UCTWorldDayTimeComponent.cpp and .h to facilitate real-time day and night cycle management in the game. Component tracks current minute, hour, and day, broadcasting events when day time and day change. Includes helper functions to determine if it's daytime and calculate total day length.

---
+++ Source/Catender02/Components/CTWorldDayTimeComponent.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CTWorldDayTimeComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams
+  (
+  	FDayTimeChanged,
+  	int, OldValue,
+  	int, NewValue,
+  	bool, OldIsDay,
+  	bool, NewIsDay
+  );
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CATENDER02_API UCTWorldDayTimeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	UDirectionalLightComponent* DirectionalLight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	int DayLength = 16;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	int NightLength = 8;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	float HourLength = 60;
+
+	int CurrentMinute = 0;
+	int CurrentHour = 6;
+	int CurrentDay = 1;
+
+	int GetDayLength() const { return DayLength + NightLength; }
+	
+public:
+	UCTWorldDayTimeComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDay();
+	
+	UFUNCTION(BlueprintAssignable)
+	FDayTimeChanged OnDayTimeChanged;
+
+	UFUNCTION(BlueprintAssignable)
+	FDayTimeChanged OnDayChanged;
+};

4ecc16c9ad977e4458e65457a9413397c1f5a0b6
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-11-12 18:29:53 +0100

    Add dynamic day/night cycle with adjustable light intensity
    Introduced functions to calculate hourly light intensity and curve value, dynamically updating the intensity of directional light based on time of day. Added debugging information for day/night state and intensity changes, and implemented smooth transitions between light intensities.

--- Source/Catender02/Components/CTWorldDayTimeComponent.cpp
+++ Source/Catender02/Components/CTWorldDayTimeComponent.cpp
@@ -1,6 +1,19 @@
 ﻿
 #include "CTWorldDayTimeComponent.h"
 
+#include "Components/DirectionalLightComponent.h"
+
+
+float UCTWorldDayTimeComponent::GetHourLightIntensity() const
+{
+	return (MaxLightIntensity - MinLightIntensity) / ((DayLength + NightLength) / 2);
+}
+
+float UCTWorldDayTimeComponent::GetHourCurveValue() const
+{
+	float FloatHour = static_cast<float>(CurrentHour) / static_cast<float>(DayLength + NightLength);
+	return CurveFloat->GetFloatValue(FloatHour);
+}
 
 UCTWorldDayTimeComponent::UCTWorldDayTimeComponent()
 {
@@ -9,21 +22,67 @@ UCTWorldDayTimeComponent::UCTWorldDayTimeComponent()
 
 void UCTWorldDayTimeComponent::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+
+	OnDayTimeChanged.AddDynamic(this, &UCTWorldDayTimeComponent::DayTimeChanged);
 }
+
+void UCTWorldDayTimeComponent::DayTimeChanged(int OldHour, int NewHour, bool OldIsDay, bool NewIsDay)
+{
+	if (OldHour == NewHour) return;
+	if (!CurveFloat) return;
+	if (!DirectionalLight) return;
+	
+	float CurveValue = GetHourCurveValue() * 10;
+	float HourLightIntensity = GetHourLightIntensity();
+
+	float NewLightIntensity = FMath::Clamp(
+		(MinLightIntensity + (HourLightIntensity * CurveValue)),
+		MinLightIntensity,
+		MaxLightIntensity
+		);
+
+	FString DebugMessage = "Day Time: " + FString::FromInt(NewHour)
+		+ " " + FString::SanitizeFloat(HourLightIntensity)
+		+ " " + FString::SanitizeFloat(GetHourCurveValue())
+		+ " " + FString::SanitizeFloat(NewLightIntensity);
+	
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		5.f,
+		FColor::Red,
+		DebugMessage
+		);
+	
+	TargetIntensity = NewLightIntensity;
+}
+
+void UCTWorldDayTimeComponent::ApplyTargetIntensity()
+{	
+	float CurrentLightIntensity = DirectionalLight->GetComponent()->Intensity;
+
+	if(CurrentLightIntensity == TargetIntensity) return;
+	
+	float Alpha = 0.005f;	
+	float NewLightIntensity = FMath::Lerp(CurrentLightIntensity, TargetIntensity, Alpha);	
+	DirectionalLight->GetComponent()->SetIntensity(NewLightIntensity);
+}
+
 
 void UCTWorldDayTimeComponent::TickComponent(float DeltaTime, ELevelTick TickType,
                                              FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	ApplyTargetIntensity();
+	
 	CurrentMinute += DeltaTime;
 
 	if (CurrentMinute < HourLength) return;
 
-	const int OldHour = CurrentMinute;
-	const int OldDay = CurrentDay;
-	const int OldIsDay = IsDay();
+	int OldHour = CurrentMinute;
+	int OldDay = CurrentDay;
+	bool OldIsDay = IsDay();
 	
 	if (CurrentMinute >= HourLength)
 	{		
@@ -33,7 +92,7 @@ void UCTWorldDayTimeComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 		OnDayTimeChanged.Broadcast(OldHour, CurrentHour, OldIsDay, IsDay());
 	}
 
-	if (CurrentMinute > GetDayLength())
+	if (CurrentHour >= GetDayLength())
 	{
 		CurrentHour = 0;
 		CurrentDay++;
@@ -44,6 +103,15 @@ void UCTWorldDayTimeComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 
 bool UCTWorldDayTimeComponent::IsDay()
 {
-	return true;
+	FString DebugMessageVariable = (GetHourCurveValue() > 0.3f) ? "true" : "false";
+	FString DebugMessage = "Is Day: " + DebugMessageVariable;
+	
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		5.f,
+		FColor::Red,
+		DebugMessage
+		);
+	return GetHourCurveValue() > 0.3f;
 }
 
--- Source/Catender02/Components/CTWorldDayTimeComponent.h
+++ Source/Catender02/Components/CTWorldDayTimeComponent.h
@@ -1,9 +1,9 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
+﻿
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/DirectionalLight.h"
 #include "CTWorldDayTimeComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams
@@ -15,31 +15,128 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams
   	bool, NewIsDay
   );
 
+/**
+ * A component that manages the simulation of day and night cycles, controlling a directional light
+ * to visually represent the progression of time in a game world.
+ */
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class CATENDER02_API UCTWorldDayTimeComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	/**
+	 * Holds a soft reference to an ADirectionalLight object which allows for
+	 * deferred loading of the light asset and provides safety for cases when
+	 * the asset isn't immediately available.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
-	UDirectionalLightComponent* DirectionalLight;
+	TSoftObjectPtr<ADirectionalLight> DirectionalLight;
 
+	/**
+	 * A reference to a curve asset used to drive a floating-point value over time, typically
+	 * for the purpose of applying time-based modifications to variables such as light intensity
+	 * throughout a day and night cycle in game environments.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
 	UCurveFloat* CurveFloat;
 
+	/**
+	 * Specifies the minimum light intensity value that the directional light can have during the day and night
+	 * cycles in the game world. This ensures that the light never goes below this intensity even during the darkest
+	 * periods, maintaining a base level of illumination for visibility and ambience.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	float MinLightIntensity = 0.1f;
+
+	/**
+	 * Defines the maximum intensity value that the directional light can reach during the simulation
+	 * of day and night cycles in the game world. This cap ensures that the light does not exceed this
+	 * intensity, maintaining a balanced visual brightness in the environment.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	float MaxLightIntensity = 10.0f;
+
+	/**
+	 * Determines the duration of the daytime cycle in the game world, affecting how long the 'day' period lasts
+	 * before transitioning into night. This value is used in conjunction with night length to control the overall
+	 * day and night cycle duration within the simulation.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
 	int DayLength = 16;
 
+	/**
+	 * Specifies the duration, in in-game hours, that the night period lasts within the day and night cycle in the game world.
+	 * This value, in combination with DayLength, determines the total cycle duration, influencing game mechanics related
+	 * to time of day.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
 	int NightLength = 8;
 
+	/**
+	 * A multiplier applied to the light intensity during nighttime, allowing for the adjustment
+	 * of how dark the environment should be when the game world transitions to night.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	float NightModifier = 0.75f;
+
+	/**
+	 * Specifies the duration (in minutes) of an in-game hour. This value determines how long one hour lasts
+	 * within the game world's simulated day and night cycle, affecting the progression speed of time in the game.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
 	float HourLength = 60;
 
-	int CurrentMinute = 0;
-	int CurrentHour = 6;
+	/**
+	 * Represents the current minute within an in-game hour, used to track the passage of time
+	 * in the game's day and night cycle simulation.
+	 */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	float CurrentMinute = 0;
+	/**
+	 * Represents the current hour in the in-game day and night cycle simulation.
+	 * Used to track the progression of time within the game world.
+	 */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	int CurrentHour = 0;
+	/**
+	 * Represents the current day within the in-game day and night cycle simulation.
+	 * Used to track the number of days that have passed in the game world.
+	 */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
 	int CurrentDay = 1;
 
+	/**
+	 * Calculates the total length of a full day cycle, which includes both the day and night periods.
+	 *
+	 * @return The combined duration of day and night in the game's time cycle.
+	 */
+	UFUNCTION()
 	int GetDayLength() const { return DayLength + NightLength; }
+
+	/**
+	 * Computes the light intensity value specific to each hour within the day and night cycle.
+	 *
+	 * @return The calculated light intensity for a particular hour based on the configured day and night durations.
+	 */
+	UFUNCTION()
+	float GetHourLightIntensity() const;
+	/**
+	 * Computes a floating-point value representing the progress of the current hour within the
+	 * entire day and night cycle based on configured durations.
+	 *
+	 * @return A normalized float value (between 0 and 1) indicating the proportion of the current
+	 * hour within the total duration of the day and night cycle.
+	 */
+	UFUNCTION()
+	float GetHourCurveValue() const;
+
+	/**
+	 * Specifies the desired light intensity for the directional light in the game world.
+	 * This value is used to adjust the brightness of the light to create varying visual
+	 * effects corresponding to different times of the day, enhancing the realism of the simulation.
+	 */
+	UPROPERTY()
+	float TargetIntensity = 0.1f;
 	
 public:
 	UCTWorldDayTimeComponent();
@@ -47,16 +144,50 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	/**
+	 * Handles changes in the game's day time, updating the directional light intensity
+	 * to reflect the transition from one hour to another within the game world.
+	 *
+	 * @param OldHour The hour representing the previous state before the change.
+	 * @param NewHour The hour representing the new state after the change.
+	 * @param OldIsDay A boolean indicating if it was day in the previous state.
+	 * @param NewIsDay A boolean indicating if it is day in the new state.
+	 */
+	UFUNCTION()
+	virtual void DayTimeChanged(int OldHour, int NewHour, bool OldIsDay, bool NewIsDay);
+
+	/**
+	 * Adjusts the directional light's intensity towards the target intensity over time.
+	 * This method gradually changes the light intensity to create a smooth transition
+	 * between the current intensity and the target intensity.
+	 */
+	UFUNCTION()
+	virtual void ApplyTargetIntensity();
+	
 public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 
-	UFUNCTION(BlueprintCallable)
+	/**
+	 * Checks if the current time in the game world is considered daytime.
+	 *
+	 * @return true if it is currently day, false otherwise.
+	 */
+	UFUNCTION(BlueprintPure)
 	bool IsDay();
-	
-	UFUNCTION(BlueprintAssignable)
+
+	/**
+	 * Delegate that is triggered whenever there is a change in the day/night cycle time.
+	 */
+	UPROPERTY(BlueprintAssignable)
 	FDayTimeChanged OnDayTimeChanged;
 
-	UFUNCTION(BlueprintAssignable)
+	/**
+	 * Delegate that is triggered whenever the day changes within the game world, allowing other systems
+	 * or components to react to the transition between days.
+	 */
+	UPROPERTY(BlueprintAssignable)
 	FDayTimeChanged OnDayChanged;
+
+	
 };

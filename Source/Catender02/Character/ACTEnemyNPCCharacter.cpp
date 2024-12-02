836f4ea3b37b450d7ecae1b5a4556b2b7fdb6443
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-02 14:56:19 +0100

    Add ACTEnemyNPCCharacter class implementation
    Introduce the ACTEnemyNPCCharacter class with basic functionality in both header and source files. This includes the constructor, Tick, BeginPlay methods, and SetupPlayerInputComponent for handling input. The class inherits from ACTCharacter and is set to execute the Tick function every frame, allowing further customization and extension in future updates.

---
+++ Source/Catender02/Character/ACTEnemyNPCCharacter.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include "ACTEnemyNPCCharacter.h"
+
+ACTEnemyNPCCharacter::ACTEnemyNPCCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ACTEnemyNPCCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACTEnemyNPCCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AACTEnemyNPCCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Catender02/Character/ACTEnemyNPCCharacter.h
@@ -0,0 +1,23 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CTCharacter.h"
+#include "ACTEnemyNPCCharacter.generated.h"
+
+UCLASS()
+class CATENDER02_API ACTEnemyNPCCharacter : public ACTCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACTEnemyNPCCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

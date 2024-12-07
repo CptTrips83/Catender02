7c39d8b733b24ce1ad1349bfdd37ba5e673a2c22
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-07 16:46:18 +0100

    Add CTWildlife class to project
    Introduce ACTWildlife class with default constructor and basic Unreal Engine lifecycle methods: BeginPlay, Tick, and SetupPlayerInputComponent. This addition lays the groundwork for handling wildlife entities within the Catender02 project, leveraging the ACTSortable base class.

---
+++ Source/Catender02/Objects/CTWildlife.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Catender02/CTSortable.h"
+#include "CTWildlife.generated.h"
+
+UCLASS()
+class CATENDER02_API ACTWildlife : public ACTSortable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACTWildlife();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

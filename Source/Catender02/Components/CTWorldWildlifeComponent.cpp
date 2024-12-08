12cf4a7b73eb1287be30ee75cce17eaf259d2d35
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-08 12:49:19 +0100

    Add CTWorldWildlifeComponent for wildlife management
    Introduce a new component, CTWorldWildlifeComponent, enabling management of wildlife within the application. This component allows adding and removing wildlife objects while broadcasting changes in the collection. It includes functions to retrieve the maximum and current number of wildlife entities.

---
+++ Source/Catender02/Components/CTWorldWildlifeComponent.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include "CTWorldWildlifeComponent.h"
+
+
+#include "Catender02/Objects/CTWildlife.h"
+
+UCTWorldWildlifeComponent::UCTWorldWildlifeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UCTWorldWildlifeComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+
+void UCTWorldWildlifeComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+int UCTWorldWildlifeComponent::GetMaxWildlife()
+{
+	return MaxWildlife;
+}
+
+int UCTWorldWildlifeComponent::GetNumWildlife()
+{
+	return Wildlife.Num();
+}
+
+void UCTWorldWildlifeComponent::AddWildlife(ACTWildlife* WildlifeToAdd)
+{
+	Wildlife.Add(WildlifeToAdd);
+	OnWildlifeChanged.Broadcast(Wildlife);
+}
+
+void UCTWorldWildlifeComponent::RemoveWildlife(ACTWildlife* WildlifeToRemove)
+{
+	Wildlife.Remove(WildlifeToRemove);
+	OnWildlifeChanged.Broadcast(Wildlife);
+}
+
---
+++ Source/Catender02/Components/CTWorldWildlifeComponent.h
@@ -0,0 +1,50 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CTWorldWildlifeComponent.generated.h"
+
+class ACTWildlife;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam
+(
+	FWildlifeChanged,
+	TArray<TSoftObjectPtr<ACTWildlife>>, Wildlife
+);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CATENDER02_API UCTWorldWildlifeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TArray<TSoftObjectPtr<ACTWildlife>> Wildlife;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	int MaxWildlife = 10;
+	
+public:
+	UCTWorldWildlifeComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintPure, Category = "Wildlife")
+	virtual int GetMaxWildlife();
+
+	UFUNCTION(BlueprintPure, Category = "Wildlife")
+	virtual int GetNumWildlife();
+	
+	UFUNCTION(BlueprintCallable, Category = "Wildlife")
+	virtual void AddWildlife(ACTWildlife* WildlifeToAdd);
+
+	UFUNCTION(BlueprintCallable, Category = "Wildlife")
+	virtual void RemoveWildlife(ACTWildlife* WildlifeToRemove);
+
+	UPROPERTY(BlueprintAssignable, Category = "Wildlife")
+	FWildlifeChanged OnWildlifeChanged;
+};

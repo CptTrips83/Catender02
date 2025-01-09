7a64a1b71d9860d7445b9150998670934bd81576
Jan-Peter Wittig <jan-peter.wittig@live.com>
2025-01-09 13:55:41 +0100

    Remove unused file references from CookerOpenOrder.log
    This change removes unnecessary file references in the CookerOpenOrder.log file, optimizing the build process and reducing clutter. The modification targets redundant assets that are no longer needed.

--- Source/Catender02.Target.cs
+++ Source/Catender02.Target.cs
@@ -8,7 +8,8 @@ public class Catender02Target : TargetRules
 	public Catender02Target(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "Catender02" } );
 	}
